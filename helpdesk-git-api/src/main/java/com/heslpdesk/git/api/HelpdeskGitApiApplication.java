ef6af79ffdc8c1717e1fbcc2facc0a4413513370
andremarinho <andre.bmarinho@hotmail.com>
2020-04-02 07:26:48 -0300

    Primeiro commit

---
+++ helpdesk-git-api/src/main/java/com/heslpdesk/git/api/HelpdeskGitApiApplication.java
@@ -0,0 +1,13 @@
+package com.heslpdesk.git.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskGitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskGitApiApplication.class, args);
+	}
+
+}
---
+++ helpdesk-git-api/src/test/java/com/heslpdesk/git/api/HelpdeskGitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heslpdesk.git.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskGitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
